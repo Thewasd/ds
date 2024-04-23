7e5772be7828dcd1d27ac234d5d6ddeacf12adb6
Thewasd <150590555+Thewasd@users.noreply.github.com>
2024-04-23 19:48:27 +0530

    Add files via upload

---
+++ Ass2/main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    System.out.println("Enter String");	
+	    String nm = sc.next();
+	    System.out.print("Server Send vahbiaV");
+	    sc.close();
+	    System.out.println();
+	}
+}
